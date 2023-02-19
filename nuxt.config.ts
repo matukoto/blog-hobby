288650dbf57d7d59ee53344065057084eb97f268
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2023-02-19 22:45:24 +0900

    feat: content の設定を追加

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,12 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxt/content']
+  modules: [
+    '@nuxt/content'
+  ],
+  content: {
+    // https://content.nuxtjs.org/api/configuration/
+    highlight: {
+      theme: 'githu-dark-dimmed',
+    },
+  },
 })
