d86a1654d8d8192c27daf77fb747bd3c47edaf9e
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-09-03 22:58:22 +0900

    feat: add lib/config.ts

---
+++ blog/src/lib/config.ts
@@ -0,0 +1,5 @@
+import { dev } from '$app/environment';
+
+export const title = 'Shakespeare';
+export const descripsion = 'SvelteKit blog for poets';
+export const url = dev ? 'http://localhost:5173' : 'https://shakespearean.vercel.app';
