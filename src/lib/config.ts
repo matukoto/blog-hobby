1cbe0f42c57b4d8724307377169d84e6fd041057
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-09-15 13:01:20 +0900

    fix: typo in config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,5 +1,5 @@
 import { dev } from '$app/environment';
 
 export const title = 'Shakespeare';
-export const descripsion = 'SvelteKit blog for poets';
+export const description = 'SvelteKit blog for poets';
 export const url = dev ? 'http://localhost:5173' : 'https://shakespearean.vercel.app';
