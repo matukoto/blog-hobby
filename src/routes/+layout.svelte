1c7f394fd6f7c45b28b4c8fe78251550dc2dbb30
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-11-25 20:58:10 +0900

    feat(flowbite):

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,51 +1,153 @@
-<script lang='ts'>
-  import Footer from './footer.svelte'
-  import Header from './header.svelte'
+<script lang="ts">
+	import '../app.postcss';
+	import type { PageData } from './$types';
+	import { page } from '$app/stores';
+	import { onMount } from 'svelte';
+	import {
+		DarkMode,
+		Navbar,
+		NavBrand,
+		NavLi,
+		NavUl,
+		NavHamburger,
+		Sidebar,
+		SidebarGroup,
+		SidebarItem,
+		SidebarWrapper,
+		Drawer,
+		CloseButton,
+		SidebarDropdownWrapper
+	} from 'flowbite-svelte';
+	import { Cog } from 'svelte-heros-v2';
+	import { sineIn } from 'svelte/easing';
 
-  import 'open-props/style'
-  import 'open-props/normalize'
-  import 'open-props/buttons'
+	let transitionParams = {
+		x: -320,
+		duration: 200,
+		easing: sineIn
+	};
+	export let data: PageData;
 
-  import '../app.css'
-  </script>
-
-    <svelte:head>
-      <script async src='https://www.googletagmanager.com/gtag/js?id=G-HN02GJ363R'></script>
-      <script>
-        window.dataLayer = window.dataLayer || [];
-        function gtag() {
-        dataLayer.push(arguments);
-        }
-        gtag('js', new Date());
-        gtag('config', 'G-HN02GJ363R');
-      </script>
-    </svelte:head>
-
-<div class='layout'>
-  <Header />
-  <main>
-    <slot />
-  </main>
-  <Footer />
-</div>
-
-<style>
-	.layout {
-		height: 100%;
-		max-inline-size: 1440px;
-		margin-inline: var(--content-margin);
-		display: grid;
-		grid-template-rows: auto 1fr auto;
-		padding-inline: var(--size-7);
+	let breakPoint: number = 1024;
+	let width: number;
+	let backdrop: boolean = false;
+	let activateClickOutside = true;
+	let drawerHidden: boolean = false;
+	$: if (width >= breakPoint) {
+		drawerHidden = false;
+		activateClickOutside = false;
+	} else {
+		drawerHidden = true;
+		activateClickOutside = true;
 	}
-
-	main {
-		padding-block: var(--size-9);
-	}
-
-	@media (min-width: 1440px) {
-		.layout {
-			padding-inline: 0;
+	onMount(() => {
+		if (width >= breakPoint) {
+			drawerHidden = false;
+			activateClickOutside = false;
+		} else {
+			drawerHidden = true;
+			activateClickOutside = true;
 		}
-	}
-</style>
+	});
+	const toggleSide = () => {
+		if (width < breakPoint) {
+			drawerHidden = !drawerHidden;
+		}
+	};
+	const toggleDrawer = () => {
+		drawerHidden = false;
+	};
+	$: activeUrl = $page.url.pathname;
+	let spanClass = 'pl-2 self-center text-md text-gray-900 whitespace-nowrap dark:text-white';
+	let divClass = 'w-full ml-auto lg:block lg:w-auto order-1 lg:order-none';
+	let ulClass =
+		'flex flex-col py-3 my-4 lg:flex-row lg:my-0 text-sm font-medium gap-4 dark:lg:bg-transparent lg:bg-white lg:border-0';
+	let navDivClass =
+		'bg-white dark:bg-gray-800 text-gray-500 dark:text-gray-400 border-gray-200 dark:border-gray-700 divide-gray-200 dark:divide-gray-700 flex items-center justify-between w-full mx-auto py-1.5 px-4';
+</script>
+
+<svelte:window bind:innerWidth={width} />
+<header class="flex-none w-full mx-auto bg-white dark:bg-slate-950">
+	<Navbar let:hidden let:toggle>
+		<NavHamburger
+			on:click={toggleDrawer}
+			btnClass="focus:outline-none whitespace-normal rounded-lg focus:ring-2 p-1.5 focus:ring-gray-400 hover:bg-gray-100 dark:hover:bg-gray-600 m-0 mr-3 lg:hidden"
+		/>
+		<NavBrand href="/" class="lg:ml-64">
+			<span class="self-center whitespace-nowrap text-xl font-semibold dark:text-white pl-4">
+        棒読みひとりごと
+			</span>
+		</NavBrand>
+		<NavUl
+			{hidden}
+			{divClass}
+			{ulClass}
+			nonActiveClass="md:!pl-3 md:!py-2 lg:!pl-0 text-gray-700 hover:bg-gray-100 lg:hover:bg-transparent lg:border-0 lg:hover:text-primary-700 dark:text-white lg:dark:hover:text-primary-700 dark:hover:bg-gray-700 dark:hover:text-white lg:dark:hover:bg-transparent"
+			activeClass="md:!pl-3 md:!py-2 lg:!pl-0 lg:text-primary-700 text-white dark:text-white dark:lg:text-primary-500 bg-primary-700 lg:bg-transparent dark:bg-primary-600 lg:dark:bg-transparent cursor-default"
+		>
+			<NavLi class="lg:px-2 lg:mb-0" active={activeUrl === '/'} href="/">Home</NavLi>
+			<NavLi class="lg:px-2 lg:mb-0" active={activeUrl === '/pages/about'} href="/pages/about"
+				>About</NavLi
+			>
+			<NavLi
+				class="lg:px-2 lg:mb-0"
+				href="https://github.com/matukoto"
+				>GitHub</NavLi
+			>
+		</NavUl>
+		<div class="flex items-center ml-auto">
+			<DarkMode class="inline-block dark:hover:text-white hover:text-gray-900" />
+		</div>
+		<NavHamburger on:click={toggle} btnClass="lg:hidden" />
+	</Navbar>
+</header>
+
+<Drawer
+	transitionType="fly"
+	{backdrop}
+	{transitionParams}
+	bind:hidden={drawerHidden}
+	bind:activateClickOutside
+	width="w-64"
+	class="overflow-scroll pb-32"
+	id="sidebar"
+>
+	<div class="flex items-center">
+		<CloseButton on:click={() => (drawerHidden = true)} class="mb-4 dark:text-white lg:hidden" />
+	</div>
+	<Sidebar asideClass="w-54">
+		<SidebarWrapper divClass="overflow-y-auto py-4 px-3 rounded dark:bg-gray-800">
+			<SidebarGroup>
+				<SidebarItem label="Home" href="/" on:click={toggleSide} active={activeUrl === `/`} />
+				{#each data.pages as { meta, path }}
+					<SidebarItem
+						label={meta.title}
+						href={`/pages/${path}`}
+						{spanClass}
+						activeClass="flex items-center p-2 text-base font-normal text-gray-900 bg-primary-200 dark:bg-primary-700 rounded-lg dark:text-white hover:bg-primary-100 dark:hover:bg-primary-700"
+						on:click={toggleSide}
+						active={activeUrl === `/pages/${path}`}
+					/>
+				{/each}
+				<SidebarDropdownWrapper label="Articles">
+					{#each data.articles as { meta, path }}
+						<SidebarItem
+							label={meta.title}
+							href={`/blog/${path}`}
+							{spanClass}
+							activeClass="flex items-center p-2 text-base font-normal text-gray-900 bg-primary-200 dark:bg-primary-700 rounded-lg dark:text-white hover:bg-primary-100 dark:hover:bg-primary-700"
+							on:click={toggleSide}
+							active={activeUrl === `/blog/${path}`}
+						/>
+					{/each}
+				</SidebarDropdownWrapper>
+			</SidebarGroup>
+		</SidebarWrapper>
+	</Sidebar>
+</Drawer>
+
+<div class="flex px-4 mx-auto w-full">
+	<main class="lg:ml-72 w-full mx-auto">
+		<slot />
+	</main>
+</div>
