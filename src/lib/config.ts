b5cc363ab2155fde786c1ab759b37a353f4c345a
Keisuke.Matsumoto <keisuke.suke.san@gmail.com>
2024-11-27 11:11:36 +0900

    feat: remove all files

--- src/lib/config.ts
+++
@@ -1,5 +0,0 @@
-import { dev } from '$app/environment'
-
-export const title = 'Top Page'
-export const description = 'SvelteKit blog for poets'
-export const url = dev ? 'http://localhost:5173' : 'https://shakespearean.vercel.app'
