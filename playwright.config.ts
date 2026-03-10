d5ef69f8f86fa53c69bd6401010bd579dc92f019
MATSUMOTO Keisuke <keisuke.suke.san@gmail.com>
2026-03-10 19:36:47 +0900

    ci: add automated test workflow

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,13 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-	webServer: { command: 'npm run build && npm run preview', port: 4173 },
-	testDir: 'e2e'
+  use: {
+    baseURL: 'http://127.0.0.1:4173',
+  },
+  webServer: {
+    command: 'pnpm dev --host 127.0.0.1 --port 4173',
+    url: 'http://127.0.0.1:4173',
+    reuseExistingServer: true,
+  },
+  testDir: 'e2e',
 });
