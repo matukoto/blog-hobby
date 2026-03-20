0ff9ffd3920725089d1427de42f962845c74376d
MATSUMOTO Keisuke <69945584+matukoto@users.noreply.github.com>
2026-03-20 23:31:53 +0900

    Fix ci cache (#701)

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   use: {
     baseURL: 'http://127.0.0.1:4173',
+    screenshot: 'only-on-failure',
   },
   webServer: {
     command: 'pnpm dev --host 127.0.0.1 --port 4173',
